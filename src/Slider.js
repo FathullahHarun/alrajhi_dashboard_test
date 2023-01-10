8158ecd3f7fe847e41ee2c4b51fc55ae5c463767
FathullahHarun <119995551+FathullahHarun@users.noreply.github.com>
2023-01-11 07:50:46 +0800

    Testing

--- src/Slider.js
+++ src/Slider.js
@@ -6,33 +6,33 @@ import image2 from "./images/Slide2.JPG";
 import "./App.css";
 
 const Slider = () => {
-	const options = {
-		type: "fade",
-		// type : "loop"
-		rewind: true,
-		gap: "1rem",
-		autoplay: true,
-		pauseOnHover: true,
-		resetProgress: true,
-		height: "100vh",
-		pagination: true,
-		arrows: false,
-		speed: 3000,
-		interval: 10000,
-	};
+  const options = {
+    // type: "fade",
+    type: "loop",
+    rewind: true,
+    gap: "1rem",
+    autoplay: true,
+    pauseOnHover: true,
+    resetProgress: true,
+    height: "100vh",
+    pagination: true,
+    arrows: false,
+    speed: 3000,
+    interval: 10000,
+  };
 
-	return (
-		<div className="container">
-			<Splide options={options} aria-label="My Favorite Images">
-				<SplideSlide>
-					<img className="slide" src={image1} alt="Picture 1" />
-				</SplideSlide>
-				<SplideSlide>
-					<img className="slide" src={image2} alt="Picture 2" />
-				</SplideSlide>
-			</Splide>
-		</div>
-	);
+  return (
+    <div className="container">
+      <Splide options={options} aria-label="My Favorite Images">
+        <SplideSlide>
+          <img className="slide" src={image1} alt="Picture 1" />
+        </SplideSlide>
+        <SplideSlide>
+          <img className="slide" src={image2} alt="Picture 2" />
+        </SplideSlide>
+      </Splide>
+    </div>
+  );
 };
 
 export default Slider;
