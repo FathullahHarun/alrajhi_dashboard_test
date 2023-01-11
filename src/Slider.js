4bb65d9ebf62844f4012207603631add0518df3d
FathullahHarun <119995551+FathullahHarun@users.noreply.github.com>
2023-01-11 11:18:14 +0800

    Testing 4

--- src/Slider.js
+++ src/Slider.js
@@ -1,35 +1,35 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import React from "react";
-import image1 from "./images/Slide1.JPG";
-import image2 from "./images/Slide2.JPG";
+import image3 from "./images/Slide3.JPG";
+// import image4 from "./images/image2.jpg";
 import "./App.css";
 
 const Slider = () => {
   const options = {
-    // type: "fade",
-    type: "loop",
+    type: "fade",
+    // type: "loop",
     rewind: true,
     gap: "1rem",
     autoplay: true,
     pauseOnHover: true,
-    resetProgress: true,
+    resetProgress: false,
     height: "100vh",
-    pagination: true,
+    pagination: false,
     arrows: false,
-    speed: 3000,
-    interval: 10000,
+    speed: 1000,
+    interval: 2000,
   };
 
   return (
     <div className="container">
       <Splide options={options} aria-label="My Favorite Images">
         <SplideSlide>
-          <img className="slide" src={image1} alt="Picture 1" />
+          <img className="slide" src={image3} alt="Picture 1" />
         </SplideSlide>
-        <SplideSlide>
-          <img className="slide" src={image2} alt="Picture 2" />
-        </SplideSlide>
+        {/* <SplideSlide>
+          <img className="slide" src={image4} alt="Picture 2" />
+        </SplideSlide> */}
       </Splide>
     </div>
   );
