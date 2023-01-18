7575cadde42e24073ba39fe863c73df705b510f8
FathullahHarun <119995551+FathullahHarun@users.noreply.github.com>
2023-01-18 16:04:41 +0800

    Images sorted

--- src/Slider.js
+++ src/Slider.js
@@ -1,9 +1,6 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import React from "react";
-// import image1 from "./images/Slide1.PNG";
-// import image2 from "./images/Slide2.PNG";
-// import image3 from "./images/Slide3.PNG";
 import { client, urlFor } from "./client";
 import { useEffect, useState } from "react";
 import "./App.css";
@@ -38,21 +35,21 @@ const Slider = () => {
       });
   }, [slides]);
 
+  images && images.sort((a, b) => (a._createdAt > b._createdAt ? 1 : -1));
+
   return (
     <div className="container">
       <Splide options={options} aria-label="My Favorite Images">
         <SplideSlide>
-          {/* <img className="slide" src={image1} alt="Picture 1" /> */}
           {images && (
             <img
               className="slide"
-              src={urlFor(images[2].image).url()}
-              alt={images[2].title}
+              src={urlFor(images[0].image).url()}
+              alt={images[0].title}
             />
           )}
         </SplideSlide>
         <SplideSlide>
-          {/* <img className="slide" src={image2} alt="Picture 2" /> */}
           {images && (
             <img
               className="slide"
@@ -62,12 +59,11 @@ const Slider = () => {
           )}
         </SplideSlide>
         <SplideSlide>
-          {/* <img className="slide" src={image3} alt="Picture 2" /> */}
           {images && (
             <img
               className="slide"
-              src={urlFor(images[0].image).url()}
-              alt={images[0].title}
+              src={urlFor(images[2].image).url()}
+              alt={images[2].title}
             />
           )}
         </SplideSlide>
